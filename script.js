1c5efd89cb7b6243c5d9e660aca9a1774035eb26
decrusader <samdewaele10@gmail.com>
2025-08-08 12:29:52 +0200

    Update script.js
    new functions

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 cookies = 0
+upgrade = 1
+limit_upgrade = 50
+while (true) {
+    document.getElementById("displaycookies").innerHTML = cookies
+    console.log(cookies)
+}
+function getcookies() {
+    return cookies
+    //kim
+}
+
 function addingnum() {
-    return ++cookies;
+    return cookies += upgrade;
+    
+}
+
+function upgradeclick() {
+    if(cookies >= limit_upgrade){
+        upgrade = upgrade * 2;
+        cookies -= limit_upgrade;
+        limit_upgrade = limit_upgrade*4
+    }
+
+    else
+        i = 1
+    //kim
 }
